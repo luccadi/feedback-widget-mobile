9fec94ca2541599cd140bffb87b33085d89bf293
LuccaDi <lucca_di@hotmail.com>
2022-05-07 18:01:49 -0300

    Creating options component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,6 +4,8 @@ import { ChatTeardropDots } from 'phosphor-react-native'
 import BottomSheet from '@gorhom/bottom-sheet'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
+import { Options } from '../Options'
+
 import { styles } from './styles'
 import { theme } from '../../theme'
 
@@ -29,8 +31,9 @@ function Widget() {
         snapPoints={[1, 280]}
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
-        children={undefined}
-      ></BottomSheet>
+      >
+        <Options />
+      </BottomSheet>
     </>
   )
 }
