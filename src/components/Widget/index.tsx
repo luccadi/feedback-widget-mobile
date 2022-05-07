c1f329330edc2ccc6d18f09ad9fc4de24969068b
LuccaDi <lucca_di@hotmail.com>
2022-05-07 20:47:49 -0300

    Creating Form component, Success component, screenshot button and feedback button

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -5,9 +5,14 @@ import BottomSheet from '@gorhom/bottom-sheet'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { Options } from '../Options'
+import { Success } from '../Success'
+import { Form } from '../Form'
 
 import { styles } from './styles'
 import { theme } from '../../theme'
+import { feedbackTypes } from '../../utils/feedbackTypes'
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null)
@@ -32,7 +37,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Success />
       </BottomSheet>
     </>
   )
