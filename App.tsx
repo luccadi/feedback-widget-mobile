290c453fff60e84315997d088ff43a0845c34ca6
LuccaDi <lucca_di@hotmail.com>
2022-05-07 13:11:38 -0300

    Installing fonts and splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+import * as SplashScreen from 'expo-splash-screen'
+
+import { theme } from './src/theme'
+import { Widget } from './src/components/Widget'
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync()
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  SplashScreen.hideAsync()
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background
+      }}
+    >
+      <Widget />
+
+      <StatusBar style="light" backgroundColor="transparent" translucent />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
